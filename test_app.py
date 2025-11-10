5f6a994a4d26af64d165570f099945bc595481d2
Ichika51 <yaroslaw.saly@yandex.ru>
2025-11-10 11:55:07 +0300

    add helth-check

--- test_app.py
+++ test_app.py
@@ -16,3 +16,10 @@ def test_hello_content(client):
     """Тест содержимого ответа"""
     response = client.get('/')
     assert response.get_data(as_text=True) == "Hello, User!"
+
+def test_health_route():
+    """Тест health check страницы"""
+    with app.test_client() as client:
+        response = client.get('/health')
+        assert response.status_code == 200
+        assert b"OK" in response.data
